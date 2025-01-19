d7c4e9c22b93500fa87d84a9b10a5dc323dbed22
1COBE1 <66527396+1COBE1@users.noreply.github.com>
2025-01-19 20:42:37 +0100

    joke.dart

--- lib/models/joke.dart
+++ lib/models/joke.dart
@@ -1,15 +1,15 @@
 class Joke {
   final String setup;
   final String punchline;
-  bool isFavorite; // Add isFavorite property
+  bool isFavorite; 
 
   Joke({
     required this.setup,
     required this.punchline,
-    this.isFavorite = false, // Default to false
+    this.isFavorite = false, 
   });
 
-  // Factory constructor to create a Joke instance from a Map
+ 
   factory Joke.fromJson(Map<String, dynamic> json) {
     return Joke(
       setup: json['setup'],
@@ -17,7 +17,7 @@ class Joke {
     );
   }
 
-  // Convert a Joke instance to a Map (useful for storing in Firebase or local storage)
+  
   Map<String, dynamic> toJson() {
     return {
       'setup': setup,
